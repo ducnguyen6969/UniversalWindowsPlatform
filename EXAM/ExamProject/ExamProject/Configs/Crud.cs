d97fa9e0431e63b44b0da1ea58b62419a9b2071d
ducnguye6969 <ducnath2010034@fpt.edu.vn>
2022-02-19 20:07:23 +0700

    update code

---
+++ EXAM/ExamProject/ExamProject/Configs/Crud.cs
@@ -0,0 +1,26 @@
+﻿using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProject.Configs
+{
+    class Crud
+    {
+          public static bool Create()
+        {
+            var conn = new SQLiteConnection("Contact.db");
+            string sql = @"CREATE TABLE IF NOT EXISTS    
+                           Contact (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                           Name VARCHAR( 140 ),
+                           PhoneNumber VARCHAR(150)) ;";
+            using (var statement = conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+            return true;
+        }
+    }
+}
